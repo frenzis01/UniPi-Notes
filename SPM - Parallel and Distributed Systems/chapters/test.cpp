03aabb9ff2b6690f9ebdf8880ed2de94c84cb0d7
Francesco Lorenzoni <francesco.lorenzoni12042@gmail.com>
2024-10-13 12:52:27 +0200

    SPM working on lect 10

---
+++ SPM - Parallel and Distributed Systems/chapters/test.cpp
@@ -0,0 +1,30 @@
+#include <cstdint>
+#include <thread>
+#include <vector>
+
+// uint64_t
+#include <iostream>  // std :: cout std :: endl
+// std :: vector
+// std: :thread
+
+// this function will be called by the threads (should be void)
+void say_hello(uint64_t id) {
+  std ::cout << "Hello from thread: " << id << std ::endl;
+}
+// this runs in the master thread
+int main(int argc, char* argv[]) {
+  const uint64_t num_threads{(argc == 2) ? std ::stoul(argv[1]) : 10};
+  std ::vector<std ::thread> threads;
+
+  // for all threads
+  for (uint64_t id = 0; id < num threads; id++)
+    // emplace the thread object in vector threads
+    // using argument forwarding, this avoids unnecessary
+    // move operations to the vector after thread creation
+    threads.emplace_back(
+        // call say hello with argument id
+        say hello, id);
+
+  // join each thread at the end
+  for (auto& thread : threads) thread.join();
+}
